aa21ef6e0c5e87383b5d2bfb2fa50a5d7a46ac96
lautaro <lautysaenzestrada@gmail.com>
2020-05-25 17:02:14 -0300

    primeros calculos con javascript

---
+++ javascript/main.js
@@ -0,0 +1,56 @@
+function tiroparabolico(){
+    var velocidad , unidadvel, unidadang, altini , angulo , velx , vely, distancia , tiempo , tiempaltmax , altmax;
+
+    velocidad = document.getElementsByName("velocidad")[0].value;
+    unidadvel = document.getElementById("unidad_velocidad").value;
+    angulo = document.getElementsByName("angulo")[0].value;
+    unidadang = document.getElementById("unidad_angulo").value;
+    altini = document.getElementsByName("altura_incial")[0].value;
+
+    if(isNaN(velocidad) || isNaN(angulo) || isNaN(altini)){
+        alert("Los datos deben ser números");
+    }
+
+    Number(velocidad);
+    Number(angulo);
+    Number(altini);
+
+    if(unidadvel === "km/h"){
+        velocidad = velocidad/3.6;
+    }
+
+    if(unidadang === "Grados"){
+        angulo = angulo * Math.PI / 180;
+    }
+
+    velx = velocidad * Math.cos(angulo);
+
+    vely = velocidad * Math.sin(angulo);
+
+    tiempo = Math.round(((altini+vely)/4.9)*100)/100;
+
+    distancia = Math.round(velx * tiempo*100)/100;
+
+    tiempaltmax = vely/9.8;
+
+    altmax = altini + (vely*tiempaltmax) - (4.9 * Math.pow(tiempaltmax , 2));
+
+    altmax = Math.round(altmax*100)/100;
+
+    document.getElementById("distancia").innerHTML = " " + distancia + " m.";
+    document.getElementById("tiempo").innerHTML = " " + tiempo + " s.";
+    document.getElementById("alturamax").innerHTML = " " + altmax + " m.";
+
+
+
+    console.log(velocidad);
+    console.log(unidadvel);
+    console.log(angulo);
+    console.log(unidadang);
+    console.log(altini);
+    console.log(velx);
+    console.log(tiempo);
+    console.log(distancia);
+    console.log(altmax);
+
+}
