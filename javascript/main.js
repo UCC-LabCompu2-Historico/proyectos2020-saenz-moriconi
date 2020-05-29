df013f5e1929cabb3eb16ba029934fd3d320d696
lautaro <lautysaenzestrada@gmail.com>
2020-05-29 03:55:31 -0300

    funcion para calcular el tiempo con altura inicial - funciones extra

--- javascript/main.js
+++ javascript/main.js
@@ -1,55 +1,86 @@
-function resetear() {
-    document.getElementById("formulario").reset();
-}
+function tiroParabolico(velocidad , unidadvel , angulo , unidadang , altini){
+    var distancia , tiempo , altmax , tiempoAltMax;
 
-function tiroparabolico(){
-    var velocidad , unidadvel, unidadang, altini , angulo , velx , vely, distancia , tiempo , tiempaltmax , altmax;
+    //reemplazo coma por punto en la variable.
+    velocidad = comaxPunto(velocidad);
+    angulo = comaxPunto(angulo);
+    altini = comaxPunto(altini);
 
-    velocidad = document.getElementsByName("velocidad")[0].value;
-    unidadvel = document.getElementById("unidad_velocidad").value;
-    angulo = document.getElementsByName("angulo")[0].value;
-    unidadang = document.getElementById("unidad_angulo").value;
-    altini = document.getElementsByName("altura_incial")[0].value;
-
-    if(velocidad.includes(",") || angulo.includes(",") || altini.includes(",")){
-        velocidad = velocidad.replace("," , ".");
-        angulo = angulo.replace("," , ".");
-        altini = altini.replace("," , ".");
-    }
-
+    //verifico si la entrada es un numero.
     if(isNaN(velocidad) || isNaN(angulo) || isNaN(altini)){
         alert("Los datos deben ser números");
     }
+
 
     Number(velocidad);
     Number(angulo);
     Number(altini);
 
+    //convierto km/h a m/s.
     if(unidadvel === "km/h"){
         velocidad = velocidad/3.6;
     }
 
+    //convierto grados a radianes.
     if(unidadang === "Grados"){
         angulo = angulo * Math.PI / 180;
     }
 
-    velx = Math.round(velocidad * Math.cos(angulo) *100)/100;
+    //calculo el tiempo final.
+    if(altini === 0){
+        tiempo = calculoTSinAI(velocidad , angulo);
+    }
+    else{
+        tiempo = calculoTConAI(velocidad , angulo , altini);
+    }
 
-    vely = Math.round(velocidad * Math.sin(angulo) *100)/100;
+    //calculo el tiempo para la altura maxima.
+    tiempoAltMax = calculoTAltMax(velocidad , angulo);
 
-    tiempo = Math.round(((altini+vely)/4.9)*100)/100;
+    //calculo la distancia final.
+    distancia = velocidad * Math.cos(angulo) * tiempo;
 
-    distancia = Math.round(velx * tiempo*100)/100;
+    //calculo la altura maxima.
+    altmax = Number(altini) /* DUDA CON NUMBER*/ + (velocidad * Math.sin(angulo) * tiempoAltMax) - (1/2 * 9.8 * Math.pow(tiempoAltMax , 2));
 
-    tiempaltmax = vely/9.8;
+    document.getElementById("distancia").innerHTML = " " + (Math.round(distancia * 100) / 100) + " m.";
+    document.getElementById("tiempo").innerHTML = " " + (Math.round(tiempo * 100) / 100) + " s.";
+    document.getElementById("alturamax").innerHTML = " " + (Math.round(altmax * 100) / 100) + " m.";
+}
 
-    altmax = altini + (vely*tiempaltmax) - (4.9 * Math.pow(tiempaltmax , 2));
+function comaxPunto(inp){
+    if(inp.includes(",")){
+        inp = inp.replace("," , ".");
+    }
+    return inp;
+}
 
-    altmax = Math.round(altmax*100)/100;
+function kmhAMs(){
 
-    document.getElementById("distancia").innerHTML = " " + distancia + " m.";
-    document.getElementById("tiempo").innerHTML = " " + tiempo + " s.";
-    document.getElementById("alturamax").innerHTML = " " + altmax + " m.";
+}
 
-    console.log(altini);
+function calculoTSinAI(velocidad , angulo) {
+    var tiempo;
+    tiempo = velocidad * Math.sin(angulo) / 4.9;
+    return tiempo;
+}
+
+function calculoTConAI(velocidad , angulo , alturaini){
+        var t1 , t2;
+        t1 = (-(velocidad * Math.sin(angulo)) + Math.sqrt(((Math.pow(velocidad * Math.sin(angulo) , 2)) + (19.6 * alturaini))))/(-9.8);
+        t2 = (-(velocidad * Math.sin(angulo)) - Math.sqrt(((Math.pow(velocidad * Math.sin(angulo) , 2)) + (19.6 * alturaini))))/(-9.8);
+        if(t1 === t2 || t1 > 0){
+            return t1;
+        }
+        return t2;
+}
+
+function calculoTAltMax(velocidad , angulo){
+    var tiempoAltMax;
+    tiempoAltMax = velocidad * Math.sin(angulo) / 9.8;
+    return tiempoAltMax;
+}
+
+function resetear() {
+    document.getElementById("formulario").reset();
 }
